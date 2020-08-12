4fa4665b8c2519524739dc3db4cfc06d67f1012a
rayfoo <18338862369@163.com>
2020-08-12 10:54:19 +0800

    放行静态资源

--- src/main/java/cn/rayfoo/common/config/InterceptorConfig.java
+++ src/main/java/cn/rayfoo/common/config/InterceptorConfig.java
@@ -3,7 +3,11 @@ package cn.rayfoo.common.config;
 import cn.rayfoo.modules.base.interceptor.JWTInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * @author rayfoo@qq.com
@@ -14,12 +18,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(new JWTInterceptor())
-        .addPathPatterns("/**")
-        .excludePathPatterns("/user/login");
+                .addPathPatterns("/**")
+                .excludePathPatterns(Arrays.asList("/","/index.html","/css/**","/js/**"))
+                .excludePathPatterns("/user/login");
 
     }
 
